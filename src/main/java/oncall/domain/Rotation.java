c62d72bda5fdc6b677542ca05c13bbd751f2f0f1
le2sky <leehaneul990623@gmail.com>
2023-12-16 17:39:06 +0900

    refactor : rotation에서 미사용 클래스 변수 제거

--- src/main/java/oncall/domain/Rotation.java
+++ src/main/java/oncall/domain/Rotation.java
@@ -12,12 +12,9 @@ public class Rotation {
     private static final String INVALID_EMPLOYEES_SIZE_MESSAGE =
             "로테이션에 포함되는 사원의 수는 최소 " + MIN_ROTATION_SIZE + "명에서 " + MAX_ROTATION_SIZE + "명입니다.";
 
-    // TODO : 이거 안쓸거야?
-    private final List<Employee> employees;
     private final Queue<Employee> rotation;
 
     private Rotation(List<Employee> employees) {
-        this.employees = employees;
         this.rotation = new LinkedList<>(employees);
     }
 
